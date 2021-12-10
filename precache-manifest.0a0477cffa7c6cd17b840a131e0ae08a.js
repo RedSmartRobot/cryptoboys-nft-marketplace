8dafe03462e68097b4b7966163c7714e8870a0b3
RedSmartRobot <sergey.budylin@gmail.com>
2021-12-10 18:16:50 +0300

    Updates

---
+++ precache-manifest.0a0477cffa7c6cd17b840a131e0ae08a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ab0821781e881fb222b",
+    "url": "/cryptoboys-nft-marketplace/static/css/main.afaf0250.chunk.css"
+  },
+  {
+    "revision": "8ab0821781e881fb222b",
+    "url": "/cryptoboys-nft-marketplace/static/js/main.8ab08217.chunk.js"
+  },
+  {
+    "revision": "4c455a24fec7090e7636",
+    "url": "/cryptoboys-nft-marketplace/static/js/1.4c455a24.chunk.js"
+  },
+  {
+    "revision": "8bf4deafcc65d566b658",
+    "url": "/cryptoboys-nft-marketplace/static/js/runtime~main.8bf4deaf.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/cryptoboys-nft-marketplace/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/cryptoboys-nft-marketplace/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "7e7596ee72d40eb52b063d267bac9ea0",
+    "url": "/cryptoboys-nft-marketplace/index.html"
+  }
+];
